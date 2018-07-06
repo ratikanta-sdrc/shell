320d5b9bca31b320c00c91275acc205b4d43118d
Ratikanta Pradhan <ratikanta131@gmail.com>
2018-07-06 15:52:43 +0530

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,8 @@
+#open angular project
+echo 'Opening script'
+svn checkout http://192.168.1.5/svn/KSP/trunk/KspWeb
+cd KspWeb
+npm i
+npm run build
+mv ksp "D:\Ratikanta\Tomcat7\webapps"
+echo 'Closing script' 
